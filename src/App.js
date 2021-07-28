4bb31799c74597c47aed9f5c542cfb8620ee70e7
Renuka Prasad <renu.yarasu@gmail.com>
2021-07-28 17:03:26 +0530

    Functional Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import { Greet } from './components/Greet';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Codevolution</h1>
+       <Greet/>
       </header>
     </div>
   );
