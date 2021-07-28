0d8362846de5378cf225478e359e43802193d19a
Renuka Prasad <renu.yarasu@gmail.com>
2021-07-28 17:25:37 +0530

    JSX

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Greet() {
     return (
-        React.createElement(
-            'div', {id:'hello', className:'hello'}, React.createElement('h1', null, 'Hello React')
-        )
+        <div>
+            <h1>HELLO JSX</h1>
+        </div>
     )
 }
