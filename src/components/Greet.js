d389b7b7927f7d76c14d7a74141fb5288b3ead68
Renuka Prasad <renu.yarasu@gmail.com>
2021-07-28 22:34:16 +0530

    Class Component - Props

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-const Greet = (props) => {
-    return (
-        <div>
-            {/* <h1>Arrow Function with Props</h1> */}
-            <h2>{props.name} a.k.a {props.lname}</h2>
-            <p>{props.children}</p>
-        </div>
-    )
+class Greet extends React.Component {
+    state = {  }
+    render() { 
+        return (  
+            // <h1>Class Component with Props</h1>
+            <h1>{this.props.name} a.k.a {this.props.lname}</h1>
+
+        );
+    }
 }
-
-export default Greet
+ 
+export default Greet;
