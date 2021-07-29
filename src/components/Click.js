c487cf6c841af3051caaf27b55cdadba629d2d06
Renuka Prasad <renu.yarasu@gmail.com>
2021-07-29 16:52:48 +0530

    Binding Event Handlers - Class Property as Arrow Function

--- src/components/Click.js
+++ src/components/Click.js
@@ -7,22 +7,28 @@ class Click extends Component {
         this.state = {
             message: 'White'
         }
-        this.clickHandler = this.clickHandler.bind(this)
+        // this.clickHandler = this.clickHandler.bind(this)
     }
-    clickHandler() {
+    /*   clickHandler() {
+          this.setState({
+              message: 'Black'
+          })
+      } */
+    clickHandler = () => {
         this.setState({
             message: 'Black'
         })
     }
-    render() {
-        return (
-            <div>
-                <h1>{this.state.message}</h1>
-                {/* <button onClick={this.clickHandler.bind(this)}>Click</button> */}
-                <button onClick={this.clickHandler}>Click</button>
-            </div >
-        )
-    }
+        render() {
+    return (
+        <div>
+            <h1>{this.state.message}</h1>
+            {/* <button onClick={this.clickHandler.bind(this)}>Click</button> */}
+            {/* <button onClick={this.clickHandler}>Click</button> */}
+            <button onClick={this.clickHandler}>Click</button>
+        </div >
+    )
+}
 }
 
 export default Click
