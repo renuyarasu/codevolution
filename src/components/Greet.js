c1ff83a5e36da4af7b5b6bf471aa0924623ee78b
Renuka Prasad <renu.yarasu@gmail.com>
2021-07-29 14:32:34 +0530

    Destructuring props - Class Component

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function Greet(props) {
-    const{name, lName} = props
-    return (
-        <div>
-            <h1>Hello {name} {lName}</h1>
-        </div>
-    )
+export default class Greet extends Component {
+    render() {
+        const {name,lName } = this.props
+        return (
+            <div>
+                <h1>Hello {name} {lName} </h1>
+            </div>
+        )
+    }
 }
