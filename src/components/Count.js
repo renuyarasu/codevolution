18f5fb3ddb680f982e6453fcccb183f26ae85f4d
Renuka Prasad <renu.yarasu@gmail.com>
2021-07-29 12:34:43 +0530

    Class Component - setState:incrementFive

--- src/components/Count.js
+++ src/components/Count.js
@@ -9,17 +9,27 @@ export default class Count extends Component {
         }
     }
     increment() {
-        this.setState({
+        /*this.setState({
             count:this.state.count +1
         }, () =>{
             console.log('Callback Value: ', this.state.count );
-        })
+        }) */
+        this.setState((prevState) => ({
+            count: prevState.count + 1
+        }))
+    }
+    incrementFive(){
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
     }
     render() {
         return (
             <div>
                 <h1>Count: {this.state.count}</h1>
-                <button style={{padding:'20px', fontWeight:'bold',cursor:'pointer'}} onClick={() => this.increment()}>Increment</button>
+                <button style={{padding:'20px', fontWeight:'bold',cursor:'pointer'}} onClick={() => this.incrementFive()}>Increment</button>
             </div>
         )
     }
