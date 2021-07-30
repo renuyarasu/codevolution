d3d78e3a76ad6c69fa3cf1d9cedcd77ec1bfa317
Renuka Prasad <renu.yarasu@gmail.com>
2021-07-30 13:25:42 +0530

    List Rendering:  Using Objects

--- src/components/Names.js
+++ src/components/Names.js
@@ -1,12 +1,40 @@
 import React from 'react'
+import Person from './Person'
 
 function Names() {
-    const namelist = ['VedaGna', 'Hindu', 'Renu']
+    // const persons = ['VedaGna', 'Hindu', 'Renu']
+
+    const persons = [
+        {
+            id: 1,
+            name: 'VedaGna',
+            age: 5,
+            skill: 'JavaScript'
+        },
+        {
+            id: 2,
+            name: 'Hindu',
+            age: 22,
+            skill: 'PHP'
+        },
+        {
+            id: 3,
+            name: 'Renu',
+            age: 35,
+            skill: 'React'
+        },
+        {
+            id: 4,
+            name: 'Yarasu',
+            age: 100,
+            skill: 'Python'
+        }
+
+    ]
+    const personsList = persons.map(person => <Person person={person}/>)
     return (
         <div>
-            {
-                namelist.map(name => <h1>{name}</h1>)
-            }
+            {personsList}
         </div>
     )
 }
