25d4e486df0fb6400cec233c6ae10376a7819d4f
Renuka Prasad <renu.yarasu@gmail.com>
2021-08-10 19:39:25 +0530

    Pure Components vs Regular Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Table from './components/Table';
+import ParentComp from './components/ParentComp';
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-              <Table/>
+              <ParentComp/>
             </header>
         </div>
     );
---
+++ src/components/ParentComp.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import PureComp from './PureComp'
+import RegComp from './RegComp'
+
+class ParentComp extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            name: 'VedaGna'
+        }
+    }
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({
+                name: 'VedaGna'
+            })
+        }, 2000)
+    }
+    render() {
+        console.log('****** | Parent Component Render | ******');
+        return (
+            <div>
+                <h1>Parent Component</h1>
+                <RegComp name={this.state.name} />
+                <PureComp name={this.state.name} />
+            </div>
+        )
+    }
+}
+
+export default ParentComp
