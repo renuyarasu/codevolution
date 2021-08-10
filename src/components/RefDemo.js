ce6619095fe8fc59688e12a8b0a07d864a10f172
Renuka Prasad <renu.yarasu@gmail.com>
2021-08-10 21:47:02 +0530

    Refs: current.value

--- src/components/RefDemo.js
+++ src/components/RefDemo.js
@@ -3,17 +3,21 @@ import React, { Component } from 'react'
 class RefDemo extends Component {
     constructor(props) {
         super(props)
-        this.inputRef = React.createRef()        
+        this.inputRef = React.createRef()
     }
-    componentDidMount(){
+    componentDidMount() {
         this.inputRef.current.focus()
         console.log(this.inputRef);
+    }
+    clickHandler =() => {
+        alert(this.inputRef.current.value)
     }
     render() {
         return (
             <div>
                 <h1>Ref Demo</h1>
-                <input type="text" ref={this.inputRef} />
+                <input type="text" ref={this.inputRef} /><br /><br />
+                <button onClick={this.clickHandler}>Click</button>
             </div>
         )
     }
