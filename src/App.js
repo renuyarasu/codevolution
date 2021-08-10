e3ed0e5e50315b01bdaa609647e16a2faaa21308
Renuka Prasad <renu.yarasu@gmail.com>
2021-08-10 11:58:37 +0530

    Submit Form

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import './App.css';
 import Form from './components/Form';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Form />
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+               <Form/>
+            </header>
+        </div>
+    );
 }
-
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,62 +1,62 @@
 import React, { Component } from 'react'
 
-export class Form extends Component {
+class Form extends Component {
     constructor(props) {
         super(props)
-
         this.state = {
             username: '',
             comments: '',
-            item: 'Bread',
+            topics: 'react'
         }
     }
-    handleUsernameChange = event => {
+    hadleUsernameChange = (event) => {
         this.setState({
             username: event.target.value
         })
     }
-    handleCommentsChange = event => {
+    hadleCommentsChange = (event) => {
         this.setState({
             comments: event.target.value
         })
     }
-    handleItemChange = event => {
+    hadleTopicsChange = (event) => {
         this.setState({
-            item: event.target.value
+            topics: event.target.value
         })
     }
-    handleSubmit = event => {
+    handleSubmit = (event) => {
         event.preventDefault()
-        alert(`${this.state.username} ${this.state.comments} ${this.state.item}`)
-
+        alert(`${this.state.username} ${this.state.comments} ${this.state.topics}`)
     }
     render() {
         return (
-            <div>
-                <h1>Form</h1>
-                <form onSubmit={this.handleSubmit}>
-                    <div>
-                        <label>User Name: </label>
-                        <input type="text" value={this.state.username} onChange={this.handleUsernameChange} />
-                    </div>
-                    <br />
-                    <div>
-                        <label>Comments: </label>
-                        <textarea value={this.state.comments} onChange={this.handleCommentsChange} cols="22" rows="3"></textarea>
-                    </div>
-                    <div>
-                        <label>Items: </label>
-                        <select value={this.state.item} onChange={this.handleItemChange}>
-                            <option value="Bread">Bread</option>
-                            <option value="Cereal">Cereal</option>
-                            <option value="Vegetables">Vegetables</option>
-                            <option value="Fruits">Fruits</option>
-                            <option value="Milk">Milk</option>
-                        </select>
-                    </div>
-                    <button type="submit">Submit</button>
-                </form>
-            </div>
+            <form onSubmit={this.handleSubmit}>
+                <table width="100%">
+                    <tbody>
+                        <tr>
+                            <td width="50%" height='50' align="left" valign="top"><label>Username: </label> </td>
+                            <td width="50%" align="left" valign="top"> <input type="text" value={this.state.username} onChange={this.hadleUsernameChange} /></td>
+                        </tr>
+                        <tr>
+                            <td align="left" valign="top"><label>Comments: </label></td>
+                            <td align="left" valign="top"><textarea cols="30" rows="5" value={this.state.comments} onChange={this.hadleCommentsChange}></textarea></td>
+                        </tr>
+                        <tr>
+                            <td align="left" height='40' valign="top"><label>Topics: </label></td>
+                            <td align="left" valign="top"><select value={this.state.topics} onChange={this.hadleTopicsChange}>
+                                <option value="react">React</option>
+                                <option value="angular">Angular</option>
+                                <option value="vue">Vue</option>
+                            </select></td>
+                        </tr>
+                        <tr>
+                            <td align="left" valign="top"><label>&nbsp; </label></td>
+                            <td align="left" valign="top"><button type="submit">Submit</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+
+            </form>
         )
     }
 }
