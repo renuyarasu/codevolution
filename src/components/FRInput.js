4773aa1488db177a028339da443ad1335ec398d0
Renuka Prasad <renu.yarasu@gmail.com>
2021-10-17 20:08:22 +0530

    Portals Demo

--- src/components/FRInput.js
+++
@@ -1,9 +0,0 @@
-import React, { forwardRef } from 'react'
-
-export const FRInput = React.forwardRef((props, ref) => {
-    return (
-        <div>
-            <input type="text" ref={ref}/>
-        </div>
-    )
-})
