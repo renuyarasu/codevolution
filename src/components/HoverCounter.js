a4f5cb38d502dbda3903e52a1e5a22cf02ebd82c
Renuka Prasad <renu.yarasu@gmail.com>
2021-10-17 21:58:55 +0530

    Higher Order Components Part 1 - Hover X Times

--- src/components/HoverCounter.js
+++ src/components/HoverCounter.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
 
 export class HoverCounter extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 0
+        }
+    }
+    incrementCount = () => {
+        this.setState(prevState => {
+            return { count: prevState.count + 1 }
+        })
+    }
     render() {
+        const { count } = this.state
         return (
             <div>
-                
+                <h1 onMouseOver={this.incrementCount}>Hover {count} Times</h1>
             </div>
         )
     }
