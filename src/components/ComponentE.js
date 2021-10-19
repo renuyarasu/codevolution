58f0f237dc49fcd112f7571563b76f291d7e4d06
Renuka Prasad <renu.yarasu@gmail.com>
2021-10-19 17:04:13 +0530

    Context - Consuming Multiple Context

--- src/components/ComponentE.js
+++ src/components/ComponentE.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import ComponentF from './ComponentF'
+import UserContext from './userContext'
 
 export class ComponentE extends Component {
+    static contextType = UserContext
     render() {
         return (
             <div>
-                <ComponentF/>
+                <h2>Component E context {this.context}</h2>
+                <ComponentF />
             </div>
         )
     }
 }
-
+// ComponentE.contextType = UserContext
 export default ComponentE
