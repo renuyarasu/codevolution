39a9d1eb84766d78715b2231abcc7676643b8d26
Renuka Prasad <renu.yarasu@gmail.com>
2021-10-20 18:01:30 +0530

    useState Hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import './App.css';
-import ClassCounter from './components/ClassCounter';
 import HookCounter from './components/HookCounter';
 
 function App() {
     return (
         <div className='App'>
-            {/* <ClassCounter/> */}
             <HookCounter/>
         </div>
     )
