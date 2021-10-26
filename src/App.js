5b70b8cfe4fbdbe13a646b574f74b6916eb50aa5
Renuka Prasad <renu.yarasu@gmail.com>
2021-10-26 06:29:21 +0530

    useCallback: Increment Components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from 'react'
 import './App.css';
-import ComponentC from './components/ComponentC';
-
-export const UserContext = React.createContext()
-export const NameContext = React.createContext()
-
+import ParentComponent from './components/ParentComponent';
 function App() {
   return (
     <div className='App'>
-      <UserContext.Provider value={'Vedana'}>
-        <NameContext.Provider  value={'Yarasu'}>
-          <ComponentC />
-        </NameContext.Provider>
-      </UserContext.Provider>
+      <ParentComponent />
     </div>
   )
 }
