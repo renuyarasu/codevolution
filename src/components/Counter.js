746b05e6a16a9197af26a52af5a811239c7d60b9
Renuka Prasad <renu.yarasu@gmail.com>
2021-10-26 12:22:45 +0530

    useMemo: Counter

---
+++ src/components/Counter.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+function Counter() {
+    const [counterOne, setCounterOne] = useState(0)
+    const [counterTwo, setCounterTwo] = useState(0)
+
+    const incrementOne = () => {
+        setCounterOne(counterOne + 1)
+    }
+    const incrementTwo = () => {
+        setCounterTwo(counterTwo + 1)
+    }
+
+    return (
+        <div>
+            <div><button onClick={incrementOne}>Counter One: {counterOne}</button></div>
+            <div><button onClick={incrementTwo}>Counter Two: {counterTwo}</button></div>
+        </div>
+    )
+}
+
+export default Counter
