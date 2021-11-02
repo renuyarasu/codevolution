5be49cfae49e9bff5307f7e755a8f8ff3aa0f464
Renuka Prasad <renu.yarasu@gmail.com>
2021-11-02 21:33:50 +0530

    Custom Hooks: useCounter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './App.css';
-import DocTitleOne from './components/DocTitleOne';
-import DocTitleTwo from './components/DocTitleTwo';
+import CounterOne from './components/CounterOne';
+import CounterTwo from './components/CounterTwo';
+
 const App = () => {
   return (
     <div className='App'>
-      <DocTitleOne />
-      <DocTitleTwo />
+      <CounterOne />
+      <CounterTwo/>
     </div>
   )
 }
---
+++ src/components/CounterOne.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import useCounter from '../hooks/useCounter'
+
+function CounterOne() {
+
+    const [count, increment, decrement, reset] = useCounter(0, 1)
+
+    return (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+            <button onClick={reset}>Reset</button>
+        </div>
+    )
+}
+
+export default CounterOne
